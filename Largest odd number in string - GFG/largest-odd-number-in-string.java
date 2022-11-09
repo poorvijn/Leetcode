014994506a194046e7b697dc1091e1437331d259
poorvijn <87180220+poorvijn@users.noreply.github.com>
2022-11-09 19:26:17 +0530

    Added solution - LeetHub

--- Largest odd number in string - GFG/largest-odd-number-in-string.java
+++ Largest odd number in string - GFG/largest-odd-number-in-string.java
@@ -24,7 +24,7 @@ class Solution {
         // code here
         int n=s.length();
         for(int i=n-1;i>=0;i--){
-            if((s.charAt(i)-'0')%2==1){
+            if((s.charAt(i))%2==1){
                 String r=s.substring(0,i+1);
                 return r;
             }
